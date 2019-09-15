98f8c18fd406b38e7171093edafee9068375cd2e
ocph23@gmail.com <ocph23@gmail.com>
2019-09-15 23:17:21 +0900

    add online db

--- api/db/dbconnection.js
+++ api/db/dbconnection.js
@@ -1,12 +1,24 @@
 const mysql = require("mysql");
 
 
+//online
 const pool = mysql.createPool({
   connectionLimit: 10,
-  password: "",
+  host:'den1.mysql1.gear.host',
+  password: "Sony@7777",
   database: "asabridb",
-  user: "root"
+  user: "asabridb"
 }); 
+
+
+// //local
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host:'localhost',
+//   password: "",
+//   database: "asabridb",
+//   user: "root"
+// }); 
 
 
 module.exports = pool;
