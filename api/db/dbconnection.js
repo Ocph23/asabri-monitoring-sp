3c2eb60b929ff267cb0c23769fe77b195a1445eb
ocph23@gmail.com <ocph23@gmail.com>
2019-10-02 16:03:45 +0900

    publish 0210-2

--- api/db/dbconnection.js
+++ api/db/dbconnection.js
@@ -2,22 +2,22 @@ const mysql = require("mysql");
 
 
 //online
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host:'den1.mysql1.gear.host',
-//   password: "Sony@7777",
-//   database: "asabridb",
-//   user: "asabridb"
-// }); 
-
-//local
 const pool = mysql.createPool({
   connectionLimit: 10,
- // host:'den1.mysql1.gear.host',
-  password: "",
+  host:'den1.mysql1.gear.host',
+  password: "Sony@7777",
   database: "asabridb",
-  user: "root"
+  user: "asabridb"
 }); 
+
+// //local
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//  // host:'den1.mysql1.gear.host',
+//   password: "",
+//   database: "asabridb",
+//   user: "root"
+// }); 
 
 
 
